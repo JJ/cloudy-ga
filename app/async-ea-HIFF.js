181b46b363fbf1f75e0255d04b5f029d32490e92
JJ Merelo <jjmerelo@gmail.com>
2016-07-11 18:44:53 +0200

    Changed to Winston for logging

--- app/async-ea-HIFF.js
+++ app/async-ea-HIFF.js
@@ -1,21 +1,27 @@
 #!/usr/bin/env node
 
 var nodeo = require('nodeo'),
-    HIFF = nodeo.HIFF,
-    fs= require('fs');
+winston = require('winston'),
+HIFF = nodeo.HIFF,
+fs= require('fs');
 
+// Load conf file
 var conf_file = process.argv[2] || 'hiff.json';
-
 var conf = JSON.parse(fs.readFileSync( conf_file, 'utf8' ));
 
-// console.log(conf);
-var log = [];
+// Set up log
+var logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)(),
+      new (winston.transports.File)({ filename: conf.output_preffix+"-hiff.log" })
+    ]
+  });
 
 if ( !conf ) {
     throw "Problems with conf file";
 }
 
-log.push( conf );
+logger.info( conf );
 var chromosome_size = conf.chromosome_size;
 var total_generations = 0;
 
@@ -25,7 +31,7 @@ var eo = new nodeo.Nodeo( { population_size: conf.population_size,
 			    chromosome_size: chromosome_size,
 			    fitness_func: hiff } );
 
-log.push( { start: process.hrtime() } );
+logger.info( { start: process.hrtime() } );
 console.log( "Starting ");
 // start running the GA
 var generation_count = 0;
@@ -39,16 +45,16 @@ function generation() {
     generation_count++;
     eo.generation();
     if ( (eo.fitness_of[eo.population[0]] < conf.fitness_max ) && (generation_count*conf.population_size < conf.max_evaluations )) {
-	console.log( eo.population[0] );
+	logger.info( { "chromosome": eo.population[0],
+		       "fitness" : eo.fitness_of[eo.population[0]]} );
 	setImmediate(generation);
     } else {
-	log.push( {end: { 
+	logger.info( {end: { 
 	    time: process.hrtime(),
 	    generation: total_generations,
 	    best : { chromosome : eo.population[0],
 		     fitness : eo.fitness_of[eo.population[0]]}}} );
 	conf.output = conf.output_preffix+".json";
-	fs.writeFileSync(conf.output, JSON.stringify(log));
 	console.log("Finished");
     }
 }
